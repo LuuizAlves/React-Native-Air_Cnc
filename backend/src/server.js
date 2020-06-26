e4ba373df1c66a453daf30c39cba226d962cadf7
Luiz Felipe <luizfelipeduartealves@outlook.com>
2020-06-25 23:14:34 -0400

    Aplicacão mobile

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,8 +7,6 @@ const routes = require('./routes');
 
 const app = express();
 
-
-
 mongoose.connect('mongodb://luizf:lfda2020@arcnc-shard-00-00-c1m0w.mongodb.net:27017,arcnc-shard-00-01-c1m0w.mongodb.net:27017,arcnc-shard-00-02-c1m0w.mongodb.net:27017/omnistack?ssl=true&replicaSet=arcnc-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
